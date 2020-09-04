8ef3306ddb988283d22cb6a6c5a2f551f8ad754b
Mustafa Shaikh <rshaikhm@gmail.com>
2020-09-04 14:19:13 +0530

    deployable

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const express = require("express");
-const redis = require("redis");
+const express = require('express');
+const redis = require('redis');
 
 const app = express();
-const client = redis.createClient({
-  host: "redis-server",
-  // port: 6379
-});
+const client = redis.createClient(
+  {host:"redis-server"});
+client.set('visits' , 0);
 
-app.get("/", (req, res) => {
-  client.get("vists", (err, visits) => {
-    res.send("No of visitors is " + visits);
-    client.set(visits, parseInt(visists) + 1);
+app.get('/', (req, res) => {
+  client.get('visits', (err, visits) => {
+    res.send('Number of visits is ' + visits);
+    client.set('visits', parseInt(visits) + 1);
   });
 });
 
-app.listen(8080);
+app.listen(8080, () => {
+  console.log('Listening on port 8080');
+});
