19529015d3c5d8d96edefb915c6f23299fed2a8c
Mustafa Shaikh <rshaikhm@gmail.com>
2020-09-04 11:39:38 +0530

    finally

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-const express = require('express');
-const redis = require('redis');
+const express = require("express");
+const redis = require("redis");
 
 const app = express();
 const client = redis.createClient({
-    host: "redis-server"
-    port: 6379
+  host: "redis-server",
+  // port: 6379
 });
 
-app.get('/', (req,res,)=> {
-
-    client.get('vists' , (err,visits)=>{
-        res.send('No of visitors is '+ visits );
-        client.set(visits , parseInt(visists)+1);
-    });
+app.get("/", (req, res) => {
+  client.get("vists", (err, visits) => {
+    res.send("No of visitors is " + visits);
+    client.set(visits, parseInt(visists) + 1);
+  });
 });
 
-app.listen(8080, () => {
-    console.log('listening on 8080');
-});
+app.listen(8080);
